428de1a61047e26ab6a53798b5f5bdf4d5346478
Yichun Zhang (agentzh) <agentzh@gmail.com>
2014-02-03 14:28:58 -0800

    added a comment.

--- lib/resty/core/base.lua
+++ lib/resty/core/base.lua
@@ -139,6 +139,7 @@ end
 
 
 function _M.get_string_buf(size)
+    -- ngx.log(ngx.ERR, "str buf size: ", str_buf_size)
     if size > str_buf_size then
         return ffi_new(c_buf_type, size)
     end
