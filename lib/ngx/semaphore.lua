b9752d1ac42e6bef22b7fbb226719ebba5093b9e
cui <523516579@qq.com>
2015-12-23 17:38:13 +0800

    Update semaphore.lua
    copyright @me, add

--- lib/ngx/semaphore.lua
+++ lib/ngx/semaphore.lua
@@ -1,4 +1,7 @@
-
+-- Copyright (C) Yichun Zhang (agentzh)
+-- Copyright (C) cuiweixie
+-- I hereby assign copyright in this code to the lua-nginx-module project,
+-- to be licensed under the same terms as the rest of the code
 local ffi = require 'ffi'
 local base = require "resty.core.base"
 
