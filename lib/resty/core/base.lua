fa45107c9b6387a5ed722910926dd936c2b33f74
cuiweixie <523516579@qq.com>
2016-01-19 16:29:25 +0800

    change: updated ngx.semaphore to reflect recent API changes in ngx_lua.
    thanks Dejiang Zhu for the tweaks.
    
    Signed-off-by: Yichun Zhang (agentzh) <agentzh@gmail.com>

--- lib/resty/core/base.lua
+++ lib/resty/core/base.lua
@@ -17,9 +17,9 @@ local FREE_LIST_REF = 0
 
 if not ngx.config
    or not ngx.config.ngx_lua_version
-   or ngx.config.ngx_lua_version < 10001
+   or ngx.config.ngx_lua_version < 10003
 then
-    error("ngx_lua 0.10.1+ required")
+    error("ngx_lua 0.10.3+ required")
 end
 
 
