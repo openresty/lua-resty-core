dc8fbb1db7681660381fcd752d450ef85f4265bd
Robert Paprocki <robert@cryptobells.com>
2017-01-05 10:41:37 -0800

    Use a lookup table for phase translation
    Also contains some style/newline fixes

--- lib/resty/core/phase.lua
+++ lib/resty/core/phase.lua
@@ -1,16 +1,35 @@
 local ffi = require 'ffi'
 local C = ffi.C
 local base = require "resty.core.base"
+local tostring = tostring
 
 ffi.cdef[[
 int ngx_http_lua_ffi_get_phase(ngx_http_request_t *r, char **err)
 ]]
 
+
 local _M = {
     _VERSION = base.version
 }
 
+
 local errmsg = base.get_errmsg_ptr()
+local context_lookup = {
+    ["1"] = "set",
+    ["2"] = "rewrite",
+    ["4"] = "access",
+    ["8"] = "content",
+    ["16"] = "log",
+    ["32"] = "header_filter",
+    ["64"] = "body_filter",
+    ["128"] = "timer",
+    ["256"] = "init_worker",
+    ["512"] = "balancer",
+    ["1024"] = "ssl_cert",
+    ["2048"] = "ssl_session_store",
+    ["4096"] = "ssl_session_fetch",
+}
+
 
 function ngx.get_phase()
     local r = getfenv(0).__ngx_req
@@ -21,38 +40,18 @@ function ngx.get_phase()
     end
 
     local context = C.ngx_http_lua_ffi_get_phase(r, errmsg)
-
     if context == -1 then -- NGX_ERROR
         error(errmsg)
-    elseif context == 0x0001 then
-        return "set"
-    elseif context == 0x0002 then
-        return "rewrite"
-    elseif context == 0x0004 then
-        return "access"
-    elseif context == 0x0008 then
-        return "content"
-    elseif context == 0x0010 then
-        return "log"
-    elseif context == 0x0020 then
-        return "header_filter"
-    elseif context == 0x0040 then
-        return "body_filter"
-    elseif context == 0x0080 then
-        return "timer"
-    elseif context == 0x0100 then
-        return "init_worker"
-    elseif context == 0x0200 then
-        return "balancer"
-    elseif context == 0x0400 then
-        return "ssl_cert"
-    elseif context == 0x0800 then
-        return "ssl_session_store"
-    elseif context == 0x1000 then
-        return "ssl_session_fetch"
-    else
-        error("unknown phase: " .. context)
     end
+
+    local ctx_str = tostring(context)
+    local phase = context_lookup[ctx_str]
+    if not phase then
+        error("unknown phase: " .. ctx_str)
+    end
+
+    return phase
 end
+
 
 return _M
