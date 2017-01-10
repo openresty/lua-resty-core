0482f5d402e97ce54fa67aab7f0dce25e1b903c9
Robert Paprocki <robert@cryptobells.com>
2017-01-09 17:36:45 -0800

    Address reviewer comments
    Context lookup table is now a sparse, integer-indexed table,
    obviating several string operations. Additionally, the base FFI_ERROR
    definition is used to check for NGX_ERROR, and tests have been
    slimmed down to explicitly check for the JIT-ability of this module.

--- lib/resty/core/phase.lua
+++ lib/resty/core/phase.lua
@@ -1,6 +1,7 @@
 local ffi = require 'ffi'
 local C = ffi.C
 local base = require "resty.core.base"
+local FFI_ERROR = base.FFI_ERROR
 local tostring = tostring
 
 ffi.cdef[[
@@ -12,42 +13,39 @@ local _M = {
     _VERSION = base.version
 }
 
-
 local errmsg = base.get_errmsg_ptr()
-local context_lookup = {
-    ["1"] = "set",
-    ["2"] = "rewrite",
-    ["4"] = "access",
-    ["8"] = "content",
-    ["16"] = "log",
-    ["32"] = "header_filter",
-    ["64"] = "body_filter",
-    ["128"] = "timer",
-    ["256"] = "init_worker",
-    ["512"] = "balancer",
-    ["1024"] = "ssl_cert",
-    ["2048"] = "ssl_session_store",
-    ["4096"] = "ssl_session_fetch",
-}
+local context_lookup = {}
+context_lookup[0x0001] = "set"
+context_lookup[0x0002] = "rewrite"
+context_lookup[0x0004] = "access"
+context_lookup[0x0008] = "content"
+context_lookup[0x0010] = "log"
+context_lookup[0x0020] = "header_filter"
+context_lookup[0x0040] = "body_filter"
+context_lookup[0x0080] = "timer"
+context_lookup[0x0100] = "init_worker"
+context_lookup[0x0200] = "balancer"
+context_lookup[0x0400] = "ssl_cert"
+context_lookup[0x0800] = "ssl_session_store"
+context_lookup[0x1000] = "ssl_session_fetch"
 
 
 function ngx.get_phase()
     local r = getfenv(0).__ngx_req
 
-    -- If we have no request object, assume we are called from the "init" phase
+    -- if we have no request object, assume we are called from the "init" phase
     if not r then
         return "init"
     end
 
     local context = C.ngx_http_lua_ffi_get_phase(r, errmsg)
-    if context == -1 then -- NGX_ERROR
+    if context == FFI_ERROR then -- NGX_ERROR
         error(errmsg)
     end
 
-    local ctx_str = tostring(context)
-    local phase = context_lookup[ctx_str]
+    local phase = context_lookup[context]
     if not phase then
-        error("unknown phase: " .. ctx_str)
+        error("unknown phase: " .. tostring(context))
     end
 
     return phase
