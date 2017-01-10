0c72e2daa7b8b394c58293854d50e6d7240ad3a8
Robert Paprocki <robert@cryptobells.com>
2017-01-09 19:10:42 -0800

    Style updates
    Bring some sanity (and capitalization) to the context lookup table.
    Also attempts to bring this module into style alignment wrt. other
    modules in this library.

--- lib/resty/core/phase.lua
+++ lib/resty/core/phase.lua
@@ -1,33 +1,32 @@
 local ffi = require 'ffi'
-local C = ffi.C
 local base = require "resty.core.base"
+
+local C = ffi.C
 local FFI_ERROR = base.FFI_ERROR
 local tostring = tostring
+
 
 ffi.cdef[[
 int ngx_http_lua_ffi_get_phase(ngx_http_request_t *r, char **err)
 ]]
 
 
-local _M = {
-    _VERSION = base.version
-}
-
 local errmsg = base.get_errmsg_ptr()
-local context_lookup = {}
-context_lookup[0x0001] = "set"
-context_lookup[0x0002] = "rewrite"
-context_lookup[0x0004] = "access"
-context_lookup[0x0008] = "content"
-context_lookup[0x0010] = "log"
-context_lookup[0x0020] = "header_filter"
-context_lookup[0x0040] = "body_filter"
-context_lookup[0x0080] = "timer"
-context_lookup[0x0100] = "init_worker"
-context_lookup[0x0200] = "balancer"
-context_lookup[0x0400] = "ssl_cert"
-context_lookup[0x0800] = "ssl_session_store"
-context_lookup[0x1000] = "ssl_session_fetch"
+local CONTEXT_LOOKUP = {
+    [0x0001] = "set",
+    [0x0002] = "rewrite",
+    [0x0004] = "access",
+    [0x0008] = "content",
+    [0x0010] = "log",
+    [0x0020] = "header_filter",
+    [0x0040] = "body_filter",
+    [0x0080] = "timer",
+    [0x0100] = "init_worker",
+    [0x0200] = "balancer",
+    [0x0400] = "ssl_cert",
+    [0x0800] = "ssl_session_store",
+    [0x1000] = "ssl_session_fetch",
+}
 
 
 function ngx.get_phase()
@@ -43,7 +42,7 @@ function ngx.get_phase()
         error(errmsg)
     end
 
-    local phase = context_lookup[context]
+    local phase = CONTEXT_LOOKUP[context]
     if not phase then
         error("unknown phase: " .. tostring(context))
     end
@@ -52,4 +51,6 @@ function ngx.get_phase()
 end
 
 
-return _M
+return {
+    version = base.version
+}
