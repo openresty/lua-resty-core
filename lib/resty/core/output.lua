ed8438ccd76252378432938667f2e12a55940459
hkmo <hkahlouche@gmail.com>
2017-01-18 14:58:47 -0500

    Update output.lua

--- lib/resty/core/output.lua
+++ lib/resty/core/output.lua
@@ -1,13 +1,11 @@
 local ffi = require "ffi"
 local base = require "resty.core.base"
 local FFI_OK = base.FFI_OK
-local ffi_cast = ffi.cast
-local const_char_ptr = ffi.typeof("const char *")
 local C = ffi.C
---local get_string_buf = base.get_string_buf
---local get_size_ptr = base.get_size_ptr
 
-local output = {}
+local _M = {
+    _VERSION = base.version
+}
 
 ffi.cdef[[
         int ngx_http_lua_ffi_write(ngx_http_request_t *r,
@@ -17,34 +15,23 @@ ffi.cdef[[
 ]]
 
 function output.ffi_write(lua_string, offset, len)
-    --local ERR_BUF_SIZE = 128
-    --local err = get_string_buf(ERR_BUF_SIZE)
-    --local errlen = get_size_ptr()
-    local buff = ffi_cast(const_char_ptr, lua_string)
-
     local r = getfenv(0).__ngx_req
 
-    if not r then
-       --return false, error("no request found")
-       ngx.log(ngx.ERR, "no request found")
-       return false
+    if not r then       
+       return false, rc
     end
 
     local rc = C.ngx_http_lua_ffi_write(r,
-                           buff,
-                           offset,
-                           len)
+                                        lua_string,
+                                        offset,
+                                        len)
 
-    if rc == FFI_OK then
-      ngx.log(ngx.INFO, "ffi_write success: code = "..rc)
+    if rc == FFI_OK then      
       return true
-    else
-      --errlen[0] = ERR_BUF_SIZE
-      --return false, error(ffi_string(err, errlen[0]))
-      ngx.log(ngx.INFO, "ffi_write error: code = "..rc)
-      return false
+    else      
+      return false, rc
     end
-
  end
 
-return output
+
+return _M
