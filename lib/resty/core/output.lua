c66b3361fe98bf7014f3e0a298abc42178e66d28
hkmo <hkahlouche@gmail.com>
2017-01-19 20:26:43 -0500

    Update output.lua

--- lib/resty/core/output.lua
+++ lib/resty/core/output.lua
@@ -1,35 +1,45 @@
 local ffi = require "ffi"
 local base = require "resty.core.base"
+
+
+local ffi_str = ffi.string
+local errmsg = base.get_errmsg_ptr()
 local FFI_OK = base.FFI_OK
-local FFI_ERROR = base.FFI_ERROR
 local C = ffi.C
+local getfenv = getfenv
+local error = error
+
 
 local _M = {
     version = base.version
 }
 
+
 ffi.cdef[[
         int ngx_http_lua_ffi_write(ngx_http_request_t *r,
                 const char* str,
                 size_t offset,
-                size_t len);
+                size_t len,
+                char **err);
 ]]
+
 
 function _M.ffi_write(lua_string, offset, len)
     local r = getfenv(0).__ngx_req
     if not r then
-        return false, FFI_ERROR
+        return error("no request found")
     end
 
     local rc = C.ngx_http_lua_ffi_write(r,
                                         lua_string,
                                         offset,
-                                        len)
+                                        len,
+                                        errmsg)
 
     if rc == FFI_OK then
         return true
     else
-        return false, rc
+        return false, ffi_str(errmsg[0])
     end
 end
 
