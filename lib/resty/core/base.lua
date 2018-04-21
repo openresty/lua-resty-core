ea113cd0db854a5663007afcc192ac611dcfb96b
Yichun Zhang (agentzh) <agentzh@gmail.com>
2018-04-20 17:18:13 -0700

    change: now we require at least ngx_lua 0.10.13.

--- lib/resty/core/base.lua
+++ lib/resty/core/base.lua
@@ -18,9 +18,9 @@ local FREE_LIST_REF = 0
 if subsystem == 'http' then
     if not ngx.config
        or not ngx.config.ngx_lua_version
-       or ngx.config.ngx_lua_version < 10012
+       or ngx.config.ngx_lua_version < 10013
     then
-        error("ngx_http_lua_module 0.10.12+ required")
+        error("ngx_http_lua_module 0.10.13+ required")
     end
 
 elseif subsystem == 'stream' then
@@ -32,7 +32,7 @@ elseif subsystem == 'stream' then
     end
 
 else
-    error("ngx_http_lua_module 0.10.12+ or "
+    error("ngx_http_lua_module 0.10.13+ or "
           .. "ngx_stream_lua_module 0.0.4+ required")
 end
 
