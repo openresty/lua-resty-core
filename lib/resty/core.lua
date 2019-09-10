56260544db150dd62c391a6f369b87c5aea27b7a
Thibault Charbonnier <thibaultcha@me.com>
2019-09-09 17:17:49 -0700

    optimize: moved 'resty.core.misc' to be loaded last to avoid metatable lookups on 'ngx'.
    This ensures that all `resty.core` modules setting values under the
    `ngx` table can do so without having to enter the `__newindex`
    metamethod set by `resty.core.misc`. This commit also includes a style
    fix.

--- lib/resty/core.lua
+++ lib/resty/core.lua
@@ -3,13 +3,11 @@
 local subsystem = ngx.config.subsystem
 
 
-require "resty.core.ctx"
 require "resty.core.var"
 require "resty.core.worker"
 require "resty.core.regex"
 require "resty.core.shdict"
 require "resty.core.time"
-require "resty.core.misc"
 require "resty.core.hash"
 require "resty.core.uri"
 require "resty.core.exit"
@@ -22,6 +20,10 @@ if subsystem == 'http' then
     require "resty.core.phase"
     require "resty.core.ndk"
 end
+
+
+require "resty.core.misc"
+require "resty.core.ctx"
 
 
 local base = require "resty.core.base"
