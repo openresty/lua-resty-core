464121e031bf4cbfb413b8c28ddc17113a9be103
Thibault Charbonnier <thibaultcha@me.com>
2020-07-13 15:41:10 -0700

    bugfix: added the missing 'ngx.req.start_time' to the stream subsystem.
    The `ngx.req.start_time()` API is part of `request.lua` which is was
    understandably not loaded for the stream subsystem. There existed no
    tests for this API in the stream subsystem, so the removal of its
    underlying CFunction resulted in `ngx.req.start_time` being `nil` with
    no test cases to realize it.

--- lib/resty/core.lua
+++ lib/resty/core.lua
@@ -12,10 +12,10 @@ require "resty.core.hash"
 require "resty.core.uri"
 require "resty.core.exit"
 require "resty.core.base64"
+require "resty.core.request"
 
 
 if subsystem == 'http' then
-    require "resty.core.request"
     require "resty.core.response"
     require "resty.core.phase"
     require "resty.core.ndk"
--- lib/resty/core/utils.lua
+++ lib/resty/core/utils.lua
@@ -3,7 +3,6 @@
 
 local ffi = require "ffi"
 local base = require "resty.core.base"
-base.allows_subsystem("http")
 
 
 local C = ffi.C
@@ -12,12 +11,7 @@ local ffi_copy = ffi.copy
 local byte = string.byte
 local str_find = string.find
 local get_string_buf = base.get_string_buf
-
-
-ffi.cdef[[
-    void ngx_http_lua_ffi_str_replace_char(unsigned char *buf, size_t len,
-        const unsigned char find, const unsigned char replace);
-]]
+local subsystem = ngx.config.subsystem
 
 
 local _M = {
@@ -25,19 +19,27 @@ local _M = {
 }
 
 
-function _M.str_replace_char(str, find, replace)
-    if not str_find(str, find, nil, true) then
-        return str
+if subsystem == "http" then
+    ffi.cdef[[
+    void ngx_http_lua_ffi_str_replace_char(unsigned char *buf, size_t len,
+        const unsigned char find, const unsigned char replace);
+    ]]
+
+
+    function _M.str_replace_char(str, find, replace)
+        if not str_find(str, find, nil, true) then
+            return str
+        end
+
+        local len = #str
+        local buf = get_string_buf(len)
+        ffi_copy(buf, str)
+
+        C.ngx_http_lua_ffi_str_replace_char(buf, len, byte(find),
+                                            byte(replace))
+
+        return ffi_str(buf, len)
     end
-
-    local len = #str
-    local buf = get_string_buf(len)
-    ffi_copy(buf, str)
-
-    C.ngx_http_lua_ffi_str_replace_char(buf, len, byte(find),
-                                        byte(replace))
-
-    return ffi_str(buf, len)
 end
 
 
